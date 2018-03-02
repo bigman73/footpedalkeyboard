330f04bee8fe42772648663f18c166f8f12c205d
Yuval Naveh <admin@helloworldacademy.com>
2018-03-02 09:13:29 -0500

    Added license Set default keyboard mappings for practice# (hardcoded for now)

--- footpedalkeyboard-driver/foodpedalkeyboard-driver.py
+++ footpedalkeyboard-driver/foodpedalkeyboard-driver.py
@@ -1,3 +1,27 @@
+""" =============================================================================
+MIT License
+
+Copyright (c) 2018 Yuval Naveh (bigman73)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+=============================================================================== """
+
 import sys
 import serial
 from serial.tools import list_ports
@@ -51,13 +75,13 @@ def process_footpedalkeyboard_event(event):
             if pedal == 'A':
                 press('l')
             elif pedal == 'B':
-                press('space')
+                press(',')
             elif pedal == 'C':
-                press('s')
+                press('space')
             elif pedal == 'D':
+                press('s')
+            elif pedal == 'E':
                 press('f')
-            else:
-                press(pedal)
 
 
 if __name__ == "__main__":
@@ -66,7 +90,9 @@ if __name__ == "__main__":
     if teensy_port:
         print("GREAT SUCCESS: Teensy found on port {0}".format(teensy_port))
         print()
-        
+
+        # TODO: Read keyboard JSON mapping file
+
         listen_to_teensy_serial(teensy_port)
     else:
         print("No compatible Teensy found. Aborting.")
