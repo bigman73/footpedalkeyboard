7d29a89ce794d25be291bd55f987ac2a6b48dff2
Yuval Naveh <admin@helloworldacademy.com>
2018-03-02 18:46:11 -0500

    Improve batch output

--- footpedalkeyboard-driver/run.bat
+++ footpedalkeyboard-driver/run.bat
@@ -2,5 +2,8 @@
 TITLE Foot Pedal Keyboard Driver
 COLOR 17
 
+ECHO Activating Conda environment: py36_fpk
 call activate py36_fpk
+
+ECHO Executing the FPK Python Driver
 python foodpedalkeyboard-driver.py
